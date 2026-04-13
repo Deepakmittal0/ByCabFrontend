4844d1caa300fbf0491bfffe85c60394499465ff
deepakmittal0 <deepakmittalji824@gmail.com>
2026-04-13 14:22:12 +0530

    change

--- src/component/whyTravel/WhyTravel.jsx
+++ src/component/whyTravel/WhyTravel.jsx
@@ -16,7 +16,7 @@ const WhyTravel = () => {
           {/* Left Image */}
           <div className="col-lg-6 mb-4 mb-lg-0">
             <div className="image-wrapper">
-              <img src="https://pub-1407f82391df4ab1951418d04be76914.r2.dev/uploads/eeda1e2d-f82f-4d90-b573-b2cb2d2d7f5b.jpeg" alt="Cab Travel" className="img-fluid rounded-img" />
+              <img src="https://pub-1407f82391df4ab1951418d04be76914.r2.dev/uploads/c1d600a4-8845-41dc-a26d-33a49afdc20d.jpeg" alt="Cab Travel" className="img-fluid rounded-img" />
             </div>
           </div>
 
